3a869efb7826d1e1ba673bc3d60cd98aee01b1ff
JoelKanyi <joelkanyi98@gmail.com>
2021-03-11 20:35:38 +0300

    finished working on signup

--- app/src/main/java/com/kanyideveloper/loginregisterscreensdemo/MainActivity.java
+++ app/src/main/java/com/kanyideveloper/loginregisterscreensdemo/MainActivity.java
@@ -2,7 +2,10 @@ package com.kanyideveloper.loginregisterscreensdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        TextView textView = findViewById(R.id.signup);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/kanyideveloper/loginregisterscreensdemo/SignUpActivity.java
+++ app/src/main/java/com/kanyideveloper/loginregisterscreensdemo/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.kanyideveloper.loginregisterscreensdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        getSupportActionBar().hide();
+
+        TextView textView = findViewById(R.id.signin);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
